0a89e6b4bb188e17ea643593c29a0bc0e0becbab
Christy Nguyen <cnguyent8901@gmail.com>
2021-11-14 19:00:24 -0800

    Added code for padding bits

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <bitset>
 using namespace std;
 
 void sha1(const string str){
@@ -19,9 +20,39 @@ void sha1(const string str){
         cout << endl;
 
         int length = binaryStr.size() * 8; //size = 1 byte or 8 bits
-        cout << "Length = " << length << endl;
+        cout << "Length = " << length << endl << endl;
         
-        //binaryStr.push_back(128);
+        binaryStr.push_back(bitset<8>(128)); //128 = 10000000 in binary. Adds the '1' bit then some of the following zero's to message
+        int numPadBits = 448 - (8 + length);
+        numPadBits = numPadBits/8; 
+        for(unsigned i = 0; i < numPadBits; i++)
+        {
+                binaryStr.push_back(bitset<8>(0));
+        }
+        cout << "Pad the message so its length is equal to 448mod512: " << endl;
+        for(unsigned i = 0; i < binaryStr.size(); i++) 
+        {
+                cout << binaryStr[i] << " ";
+        }
+        cout << endl;
+
+        bitset<64> binaryLength = bitset<64>(length);   //a 64 bit value which represents the original length        
+        //break up 64 bit bitset into 8 bits so they can be put in vector
+        for(int i = 56; i >= 8; i = i - 8)
+        {
+                binaryStr.push_back(bitset<8>((binaryLength >> i).to_ulong()));
+        }
+        binaryStr.push_back(bitset<8>(binaryLength.to_ulong()));
+
+        cout << "64-bit value is added to the message: " << endl;
+        for(unsigned i = 0; i < binaryStr.size(); i++) 
+        {
+                cout << binaryStr[i] << " ";
+        }
+        cout << endl;
+
+        // length = binaryStr.size() * 8; //size = 1 byte or 8 bits
+        // cout << "Length = " << length << endl;
 
         // string testString = binaryStr[0].to_string();
         // cout << "Test: " << testString << endl;
@@ -32,5 +63,13 @@ void sha1(const string str){
 int main() {
         string str = "This is a string for testing the hash function";
         sha1(str);
+
+        // string input;
+        // while(1)
+        // {
+        //         cout << "Enter a string to hash: ";
+        //         cin >> input;
+        //         sha1(input);
+        // }
         return 0;
 }
