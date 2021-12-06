05fe3a9cc98d67a925c3ca02d6f0ebb7747c5cf2
ajay-walia <89554524+ajay-walia@users.noreply.github.com>
2021-12-05 16:47:33 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,3 +5,42 @@ int main() {
     test.set();
     test.hash(test);
 }
+#include "SHA1.h"
+#include "md5.h"
+#include "SHA2-256.h"
+#include "SHA3-256.h"
+
+int main() {
+
+    //SHA1
+    SHA1 sha1;
+    sha1.set();
+    sha1.hash(sha1);
+
+    //MD5
+    MD5 md5;
+    string input_md5;
+    cout << "(MD5)Enter a string to hash: ";
+    cin >> input_md5;
+    string myHash = md5(input_md5);
+    cout << "Result = " << myHash << endl << endl;
+
+    //SHA2_256
+    SHA256 sha2_256;
+    string input_sha2;
+    cout << "(SHA2)Enter a string to hash: ";
+    cin >> input_sha2;
+    string myHash2 = sha2_256(input_sha2);
+    cout << "Result = " << myHash2 << endl << endl;
+    
+
+    //SHA3_256
+    SHA3 sha3_256;
+    string input_sha3;
+    cout << "(SHA3)Enter a string to hash: ";
+    cin >> input_sha3;
+    string myHash3 = sha3_256(input_sha3);
+    cout << "Result = " << myHash3 << endl << endl;
+
+    return 0;
+}
